83d0b6bab5e7e9c1fc6982ddc0059033dba92156
PKMNPlatin <platinsoul01@gmail.com>
2022-07-04 22:11:55 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "gphoto2pp/helper_camera_wrapper.hpp"
+#include "gphoto2pp/camera_wrapper.hpp"
+#include "gphoto2pp/camera_list_wrapper.hpp"
+
+#include "gphoto2pp/exceptions.hpp"
+
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+
+}
