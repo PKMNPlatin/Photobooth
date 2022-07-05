17e1a0294cc3b9a0e9da79a6dee8f42dcdc4fa81
PKMNPlatin <platinsoul01@gmail.com>
2022-07-05 03:37:07 +0200

    Revived old test

--- src/gpio.cpp
+++ src/gpio.cpp
@@ -8,27 +8,27 @@
 
 namespace photobooth {
 
-    void gpio::setupGPIOPins() {
-        wiringPiSetup();
-        pinMode(GPIO_PIN_PREV, INPUT);
-        pinMode(GPIO_PIN_PRINT, INPUT);
-        pinMode(GPIO_PIN_NEXT, INPUT);
-        pinMode(GPIO_PIN_CAPTURE, INPUT);
-    }
-
-    void gpio::readCurrentState() {
-        if(digitalRead(GPIO_PIN_PREV)) {
-            std::cout << "Pressed prev button!" << std::endl;
-        }
-        if(digitalRead(GPIO_PIN_PRINT)) {
-            std::cout << "Pressed print button!" << std::endl;
-        }
-        if(digitalRead(GPIO_PIN_NEXT)) {
-            std::cout << "Pressed next button!" << std::endl;
-        }
-        if(digitalRead(GPIO_PIN_CAPTURE)) {
-            std::cout << "Pressed capture button!" << std::endl;
-        }
-    }
+//    void gpio::setupGPIOPins() {
+//        wiringPiSetup();
+//        pinMode(GPIO_PIN_PREV, INPUT);
+//        pinMode(GPIO_PIN_PRINT, INPUT);
+//        pinMode(GPIO_PIN_NEXT, INPUT);
+//        pinMode(GPIO_PIN_CAPTURE, INPUT);
+//    }
+//
+//    void gpio::readCurrentState() {
+//        if(digitalRead(GPIO_PIN_PREV)) {
+//            std::cout << "Pressed prev button!" << std::endl;
+//        }
+//        if(digitalRead(GPIO_PIN_PRINT)) {
+//            std::cout << "Pressed print button!" << std::endl;
+//        }
+//        if(digitalRead(GPIO_PIN_NEXT)) {
+//            std::cout << "Pressed next button!" << std::endl;
+//        }
+//        if(digitalRead(GPIO_PIN_CAPTURE)) {
+//            std::cout << "Pressed capture button!" << std::endl;
+//        }
+//    }
 
 } // photobooth
--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,34 @@
 #include <gphoto2/gphoto2.h>
 #include <iostream>
 
-#include "gpio.h"
+//#include "gpio.h"
 
 static void onGPhotoError(GPLogLevel level, const char *domain, const char *str, void* data) {
     fprintf(stdout, "%s\n", str);
 }
 
 int main(int argc, char* argv[]) {
-//    int retVal;
-//    Camera *pCamera;
-//    GPContext *pGpContext = gp_context_new();
-//
-////    gp_log_add_func(GP_LOG_DEBUG, onGPhotoError, NULL);
-//    gp_camera_new(&pCamera);
-//https://discord.com/channels/@me/419937058178596864
-//    printf("Camera initializing...\n");
-//
-//    retVal = gp_camera_init(pCamera, pGpContext);
-//    if(retVal != GP_OK) {
-//        std::cout << "Error while initializing the camera. Exiting..." << std::endl;
-//        exit(1);
-//    }
-//    std::cout << "Successfully initialized a camera!" << std::endl;
-//    gp_camera_exit(pCamera, pGpContext);
+    int retVal;
+    Camera *pCamera;
+    GPContext *pGpContext = gp_context_new();
 
-    photobooth::gpio gpio;
-    gpio.setupGPIOPins();
+    gp_camera_new(&pCamera);
+    printf("Camera initializing...\n");
 
-    while(true) {
-        gpio.readCurrentState();
+    retVal = gp_camera_init(pCamera, pGpContext);
+    if(retVal != GP_OK) {
+        std::cout << "Error while initializing the camera. Exiting..." << std::endl;
+        exit(1);
     }
+    std::cout << "Successfully initialized a camera!" << std::endl;
+    gp_camera_exit(pCamera, pGpContext);
+
+//    photobooth::gpio gpio;
+//    gpio.setupGPIOPins();
+//
+//    while(true) {
+//        gpio.readCurrentState();
+//    }
     return 0;
 }
 
