ccc7b0c81fb311a7e2a489830e4d78d86d2d5ceb
PKMNPlatin <platinsoul01@gmail.com>
2022-07-05 03:13:56 +0200

    Simple gpio test

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,36 @@
 #include <gphoto2/gphoto2.h>
 #include <iostream>
 
+#include "gpio.h"
+
 static void onGPhotoError(GPLogLevel level, const char *domain, const char *str, void* data) {
     fprintf(stdout, "%s\n", str);
 }
 
 int main(int argc, char* argv[]) {
-    int retVal;
-    Camera *pCamera;
-    GPContext *pGpContext = gp_context_new();
+//    int retVal;
+//    Camera *pCamera;
+//    GPContext *pGpContext = gp_context_new();
+//
+////    gp_log_add_func(GP_LOG_DEBUG, onGPhotoError, NULL);
+//    gp_camera_new(&pCamera);
+//
+//    printf("Camera initializing...\n");
+//
+//    retVal = gp_camera_init(pCamera, pGpContext);
+//    if(retVal != GP_OK) {
+//        std::cout << "Error while initializing the camera. Exiting..." << std::endl;
+//        exit(1);
+//    }
+//    std::cout << "Successfully initialized a camera!" << std::endl;
+//    gp_camera_exit(pCamera, pGpContext);
 
-//    gp_log_add_func(GP_LOG_DEBUG, onGPhotoError, NULL);
-    gp_camera_new(&pCamera);
+    photobooth::gpio gpio;
+    gpio.setupGPIOPins();
 
-    printf("Camera initializing...\n");
-
-    retVal = gp_camera_init(pCamera, pGpContext);
-    if(retVal != GP_OK) {
-        std::cout << "Error while initializing the camera. Exiting..." << std::endl;
-        exit(1);
+    while(true) {
+        gpio.readCurrentState();
     }
-    std::cout << "Successfully initialized a camera!" << std::endl;
-    gp_camera_exit(pCamera, pGpContext);
     return 0;
 }
 
