025f2aa066f144d608727c5305a5c9cbaba20808
PKMNPlatin <platinsoul01@gmail.com>
2022-07-06 21:22:17 +0200

    Added test shutter function

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-#include <csignal>
 #include "GPIO.h"
 #include "gphoto2pp/camera_wrapper.hpp"
+#include "gphoto2pp/camera_file_wrapper.hpp"
+#include "gphoto2pp/helper_camera_wrapper.hpp"
 
 int main(int argc, char *argv[]) {
     gphoto2pp::CameraWrapper camera;
@@ -9,15 +10,16 @@ int main(int argc, char *argv[]) {
     std::cout << camera.getSummary() << std::endl;
     photobooth::GPIO GPIO;
     GPIO.registerGPIOPins();
+    int i = 0;
     while (true) {
         GPIO.checkPinState();
 
         auto capturePin = GPIO.getTasterByPin(5);
         if (capturePin && capturePin->getState()) {
-
+            gphoto2pp::CameraFileWrapper fileWrapper;
+            gphoto2pp::helper::capture(camera, fileWrapper);
+            fileWrapper.save("Test"+ std::to_string(i)+".png");
         }
-        sleep(3);
-
     }
     return 0;
 }
